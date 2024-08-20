3a8a665c7c8c09c910cfaae0f03fee1350a074d5
juuniorsoares <139187475+juuniorsoares@users.noreply.github.com>
2024-08-20 20:45:28 -0300

    aula sobre javascript

---
+++ aula09/script.js
@@ -0,0 +1,4 @@
+console.log("Ola, mundo!");
+console.log("Revisando conteudo sobre JavaScript");
+console.log("Sou um progrmador de sucesso!")
+console.log("Erivaldo" + " Soares" + " Junior" + " :" + " 2004");
