65defd28c1199581a88ce79ea7629e380852b7a7
juuniorsoares <139187475+juuniorsoares@users.noreply.github.com>
2024-08-27 21:48:37 -0300

    exercicios da aula09

--- exercicios/aula09/script.js
+++ exercicios/aula09/script.js
@@ -1,4 +1,103 @@
-console.log("Ola, mundo!");
-console.log("Revisando conteudo sobre JavaScript");
-console.log("Sou um progrmador de sucesso!")
-console.log("Erivaldo" + " Soares" + " Junior" + " :" + " 2004");
+// var globalGreeting = "Good ";
+
+// function testFunction() {
+//     var localGreeting = "Morning ";
+//     console.log("function:");
+//     console.log(globalGreeting);
+//     console.log(localGreeting);
+// }
+
+// testFunction();
+
+// console.log("main program:");
+// console.log(globalGreeting);
+// console.log(localGreeting);
+
+// console.log("----------------------------");
+
+// let counter = 200;
+// console.log(counter);
+
+// {
+//     let counter = 300;
+//     console.log(counter);
+// }
+
+// console.log(counter)
+
+// console.log("---------------------");
+
+// 
+
+// console.log("----------------------------")
+
+// let counter = 200;
+
+// function functiontest(){
+//     let counter = 400;
+//     console.log(counter);
+// }
+
+// console.log(counter);
+// functiontest();
+// console.log(counter);
+
+// console.log("---------------------------");
+
+// var height = 180;
+// console.log(height);
+// console.log(weight);
+// var weight = 70;
+// console.log(weight);
+
+// console.log('-------------------------------');
+
+// let precorosa = 8;
+// let precolily = 10;
+// let precotulipa = 2;
+
+// let qntdrosa = 70;
+// let qntdlily = 50;
+// let qntdtulipa = 120;
+
+// let rosavalor = precorosa * qntdrosa;
+// let lilyvalor = precolily * qntdlily;
+// let tulipavalor = precotulipa * qntdtulipa;
+
+// let total =  rosavalor + lilyvalor + tulipavalor;
+
+// console.log("rosas - preço unitario: ", precorosa , "quantidade de rosas: ", qntdrosa , "valor total: " , rosavalor);
+// console.log("lily - preço unitario: ", precolily , "quantidade de lily: ", qntdlily , "valor total: " , lilyvalor);
+// console.log("tulipa - preço unitario: ", precotulipa , "quantidade de tulipa: ", qntdtulipa , "valor total: " , tulipavalor);
+// console.log("Total: ", total);
+
+// console.log("----------------------------")
+
+// qntdrosa = qntdrosa - 20;
+// qntdlily = qntdlily - 30;
+
+// rosavalor = precorosa * qntdrosa;
+// lilyvalor = precolily * qntdlily;
+// tulipavalor = precotulipa * qntdtulipa;
+
+// total =  rosavalor + lilyvalor + tulipavalor;
+
+// console.log("rosas - preço unitario: ", precorosa , "quantidade de rosas: ", qntdrosa , "valor total: " , rosavalor);
+// console.log("lily - preço unitario: ", precolily , "quantidade de lily: ", qntdlily , "valor total: " , lilyvalor);
+// console.log("tulipa - preço unitario: ", precotulipa , "quantidade de tulipa: ", qntdtulipa , "valor total: " , tulipavalor);
+// console.log("Total: ", total);
+
+// console.log('-------------------------------');
+
+let nome = "Erivaldo Soares Junior";
+let tel = "98 98477-9064";
+let email = "js4617809@gmail.com";
+console.log("nome:", nome);
+console.log("telefone:" , tel);
+console.log("email:", email);
+
+// console.log('-------------------------------');
+
+
+
+
