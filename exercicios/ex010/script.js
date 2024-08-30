e91668bba95324b3dfb0f77ebad30db59dfa930a
juuniorsoares <139187475+juuniorsoares@users.noreply.github.com>
2024-08-29 21:11:54 -0300

    exercicios de conversões

---
+++ exercicios/ex010/script.js
@@ -0,0 +1,35 @@
+
+// EXERCICIOS 01
+
+// let str = "texto";
+// let str2 = String(str);
+// console.log(`str ${str} e o tipo ${typeof str2}`);
+
+// let bl = true;
+// let bl2 = Boolean(true);
+// console.log(`booleano ${bl} e o tipo ${typeof bl2}`);
+
+// let n = 41;
+// let n2 = Number(n);
+// console.log(`number ${n} e o tipo ${ typeof n2}`);
+
+// let bi = 10n;
+// let bi2 = BigInt(10n);
+// console.log(`BigInt ${bi} e o tipo ${ typeof bi2}`);
+
+// let ul = undefined;
+// console.log(`${ul} e o tipo ${typeof ul}`);
+
+// EXERCICIO 02
+
+// let b = true + false;
+// let n = 100 + 200;
+// let bi = 100n + 200n;
+// let s = "He" + "llo";
+// let u = undefined + undefined;
+
+// console.log(`${b} [${typeof b}]`); // !!! number 
+// console.log(`${n} [${typeof n}]`);
+// console.log(`${bi} [${typeof bi}]`);
+// console.log(`${s} [${typeof s}]`);
+// console.log(`${u} [${typeof u}]`); // !!! number
