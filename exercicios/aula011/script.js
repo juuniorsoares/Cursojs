5e558d2116afe8e67921f6c54e6ad138388bb3e4
juuniorsoares <139187475+juuniorsoares@users.noreply.github.com>
2024-09-27 15:40:22 -0300

    aula sobre objetos

---
+++ exercicios/aula011/script.js
@@ -0,0 +1,30 @@
+// SEM USAR OBJETOS 
+
+// let name1 = "Calvin";
+// let surname1 = "Hart";
+// let age1 = 66;
+// let email1 = "CalvinMHart@teleworm.us";
+//
+// let name2 = "Mateus";
+// let surname2 = "Pinto";
+// let age2 = 21;
+// let email2 = "MateusPinto@dayrep.com";
+
+
+// USANDO OBEJETOS
+
+// let user1 = {
+//     name: "Calvin",
+//     surname: "Hart",
+//     age: 66,
+//     email: "CalvinMHart@teleworm.us"
+// };
+// console.log(user1.name, user1.surname);
+
+// let user2 = {
+//     name: "Mateus",
+//     surname: "Pinto",
+//     age: 21,
+//     email: "MateusPinto@dayrep.com"
+// };
+// console.log(user2.name, user2.surname)
