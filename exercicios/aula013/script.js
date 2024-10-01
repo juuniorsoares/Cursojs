fb66728eec4560a80650d41547b7d8301b516256
juuniorsoares <139187475+juuniorsoares@users.noreply.github.com>
2024-09-30 21:53:38 -0300

    aula sobre operadores

---
+++ exercicios/aula013/script.js
@@ -0,0 +1,28 @@
+// Operadores unários de incremento e decremento
+
+// let n1 = 10;
+// let n2 = 10;
+
+// // Esses operadores na versão pós-fixada (ou seja, o operador está no lado direito do operando) executam a operação alterando o valor da variável, mas retornam o valor antes da alteração. 
+// console.log(n1); // -> 10
+// console.log(n1++); // -> 10
+// console.log(n1); // -> 11
+
+// // A versão prefixada do operador (ou seja, o operador é colocado antes do operando) executa a operação e retorna o novo valor.
+// console.log(n2); // -> 10
+// console.log(++n2); // -> 11
+// console.log(n2); // -> 11
+
+// USANDO OPERADORES DE COMPARAÇÃO 
+
+// console.log((2 * 3) + 1)
+// console.log(2 ** 4)
+// console.log(5 * 1)
+// console.log(8 ** 2 - 5 ** 2)
+
+// USANDO OPERADORES LOGICOS
+
+// console.log( true || false)
+// console.log(false || ! false)
+// console.log(false || false || true)
+// console.log(true || false && false && true)
