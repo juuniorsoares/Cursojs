430e842e941d9d8312a3dc6acedc3e5d0cefb44f
juuniorsoares <139187475+juuniorsoares@users.noreply.github.com>
2024-10-01 21:50:12 -0300

    aula014 sobre caixa de diálogos

---
+++ exercicios/aula015/script.js
@@ -0,0 +1,43 @@
+// USANDO O ALERT():
+
+// alert("Hello, World!")
+// window.alert("Hello, World! for the second time");
+// alert(4 * 7);
+// alert(true);
+// alert("text 1", "text 2"); // only "text 1" will be displayed
+
+
+// USANDO O CONFIRM():
+
+// let remove = confirm("Remove all data?");
+// let message = remove ? "Deleting Data" : "Cancelled"
+// console.log(message);
+
+
+// USANDO O PROMPT():
+
+// let name = window.prompt("What is your name?", "John Doe");
+// name = name ? name : "anonymous";
+// let age = prompt("Hello " + name + " how old are you?");
+// alert(name + " is " + age + " years old");
+
+// EXERCICIO 
+
+// let a = window.prompt("Qual a Altura ?", 0);
+// let c = window.prompt("Qual é o Comprimento?", 0)
+// let l = window.prompt("Qual é a largura?", 0 )
+// let v = a * c * l;
+
+// let resultado = window.alert(`O volume da caixa é ${v}`)
+
+//  RESOLUÇÃO DE EXERCICIOS
+
+// let n = 2 * 3 ** 3 - 1;
+// console.log(n)
+
+// let n = 10;
+// let m = ++n;
+// console.log(n)
+// console.log(m)
+
+// let test = prompt("Hello", "World");
