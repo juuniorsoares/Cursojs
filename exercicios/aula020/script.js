15a175491de3cb243a269e966e2d721d6228d599
juuniorsoares <139187475+juuniorsoares@users.noreply.github.com>
2024-11-05 21:45:46 -0300

    aula sobre tipos de dados simples

---
+++ exercicios/aula020/script.js
@@ -0,0 +1,23 @@
+// let numberObj1 = new Number;            // -> 0
+// let numberObj2 = new Number(100);       // -> 100
+// let numberObj3 = Number("200");     // -> 200
+// let numberObj4 = Number("abcd");    // -> NaN
+// let numberObj5 = Number(9e10000);   // -> Infinity
+// console.log(`numberObj5 : ${typeof numberObj5} : ${ numberObj5.valueOf()}`);
+
+
+// DIVIDINDO STRINGS COM O METODO SPLIT:
+
+// let ipAddressStr = "127.0.0.1"
+// let ipParts = ipAddressStr.split("."); // -> ["127", "0", "0", "1"]
+// console.log(ipParts[0]); // -> 127
+
+
+// PROCURANDO POR STRINGS: INCLUDES, LASTINDEXOF, INDEXOF:
+
+// console.log(text.includes("two")); // -> true
+// console.log(text.includes("four")); // -> false
+// console.log(text.indexOf("two")); // -> 5
+// console.log(text.indexOf("four")); // -> -1
+// console.log(text.lastIndexOf("two")); // -> 22
+
